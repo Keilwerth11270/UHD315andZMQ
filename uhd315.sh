ba5ef5fdb9bc26c1b772e597f2d81141e9a5a97e
Fahim Bashar <74380584+Keilwerth11270@users.noreply.github.com>
2024-10-18 16:14:40 -0400

    Update uhd315.sh

--- uhd315.sh
+++ uhd315.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-
 # Bash script to install UHD version 3.15.0.0
-
-cd
 
 # Exit script if any command fails
 set -e
 
 # Clone the UHD repository
-echo ""
-echo ""
-echo ""
-echo "Cloning UHD repository..."
+echo -e "\n\nCloning UHD repository..."
 sleep 3
 git clone https://github.com/EttusResearch/uhd.git
 
@@ -19,43 +13,34 @@ git clone https://github.com/EttusResearch/uhd.git
 cd uhd
 
 # Checkout the specified version
-echo ""
-echo ""
-echo ""
-echo "Checking out UHD version 3.15.0.0..."
+echo -e "\n\nChecking out UHD version 3.15.0.0..."
 sleep 3
 git checkout v3.15.0.0
 
 # Change directory to host and prepare for building
-echo ""
-echo ""
-echo ""
+echo -e "\n\nPreparing build environment..."
 cd host
 mkdir build
 cd build
 
+# Set compiler to gcc-10 and g++-10
+echo -e "\n\nSetting compiler to GCC 10..."
+export CC=$(which gcc-10)
+export CXX=$(which g++-10)
+
 # Generate Makefiles with CMake, build, and install
-echo ""
-echo ""
-echo ""
-echo "Configuring, building, and installing UHD..."
+echo -e "\n\nConfiguring, building, and installing UHD..."
 sleep 3
-cmake ..
-make
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
 
 # Install UHD (requires root privileges)
-echo ""
-echo ""
-echo ""
-echo "Installing UHD, please enter your password if prompted..."
+echo -e "\n\nInstalling UHD, please enter your password if prompted..."
 sleep 3
 sudo make install
 
 # Update the shared library cache
-echo ""
-echo ""
-echo ""
-echo "Updating the shared library cache..."
+echo -e "\n\nUpdating the shared library cache..."
 sleep 3
 sudo ldconfig
 
